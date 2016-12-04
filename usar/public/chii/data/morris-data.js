41d4aac6d7803f458109760914b9330b7eb506e1
Jason Huang <jasson15@gmail.com>
2016-12-04 01:59:14 -0800

    Refine chart

--- usar/public/chii/data/morris-data.js
+++ usar/public/chii/data/morris-data.js
@@ -1,117 +1,66 @@
 $(function() {
 
-    Morris.Area({
+    Morris.Line({
         element: 'morris-area-chart',
         data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
+            period: '2016-12-04T09:30:00',
+            pulse: 126.66,
+            lowerbound: 40,
+            upperbound: 180,
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            period: '2016-12-04T09:31:00',
+            pulse: 127.78,
+            lowerbound: 40,
+            upperbound: 180,
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
+            period: '2016-12-04T09:32:00',
+            pulse: 49.12,
+            lowerbound: 40,
+            upperbound: 180,
         }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
+            period: '2016-12-04T09:33:00',
+            pulse: 137.67,
+            lowerbound: 40,
+            upperbound: 180,
         }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
+            period: '2016-12-04T09:34:00',
+            pulse: 68.10,
+            lowerbound: 40,
+            upperbound: 180,
         }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
+            period: '2016-12-04T09:35:00',
+            pulse: 56.70,
+            lowerbound: 40,
+            upperbound: 180,
         }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
+            period: '2016-12-04T09:36:00',
+            pulse: 48.20,
+            lowerbound: 40,
+            upperbound: 180,
         }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
+            period: '2016-12-04T09:37:00',
+            pulse: 150.73,
+            lowerbound: 40,
+            upperbound: 180,
         }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
+            period: '2016-12-04T09:38:00',
+            pulse: 106.87,
+            lowerbound: 40,
+            upperbound: 180,
         }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            period: '2016-12-04T09:39:00',
+            pulse: 84.32,
+            lowerbound: 40,
+            upperbound: 180,
         }],
         xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
+        ykeys: ['pulse', 'lowerbound', 'upperbound'],
+        labels: ['Pulse', 'Lowerbound', 'Upperbound'],
+        yLabelFormat: function(y) { return y + ' bpm' },
+        lineColors: ['#0b62a4', '#edc240', '#cb4b4b'],
         pointSize: 2,
         hideHover: 'auto',
         resize: true
     });
-
-    // Morris.Donut({
-    //     element: 'morris-donut-chart',
-    //     data: [{
-    //         label: "Download Sales",
-    //         value: 12
-    //     }, {
-    //         label: "In-Store Sales",
-    //         value: 30
-    //     }, {
-    //         label: "Mail-Order Sales",
-    //         value: 20
-    //     }],
-    //     resize: true
-    // });
-
-    // Morris.Bar({
-    //     element: 'morris-bar-chart',
-    //     data: [{
-    //         y: '2006',
-    //         a: 100,
-    //         b: 90
-    //     }, {
-    //         y: '2007',
-    //         a: 75,
-    //         b: 65
-    //     }, {
-    //         y: '2008',
-    //         a: 50,
-    //         b: 40
-    //     }, {
-    //         y: '2009',
-    //         a: 75,
-    //         b: 65
-    //     }, {
-    //         y: '2010',
-    //         a: 50,
-    //         b: 40
-    //     }, {
-    //         y: '2011',
-    //         a: 75,
-    //         b: 65
-    //     }, {
-    //         y: '2012',
-    //         a: 100,
-    //         b: 90
-    //     }],
-    //     xkey: 'y',
-    //     ykeys: ['a', 'b'],
-    //     labels: ['Series A', 'Series B'],
-    //     hideHover: 'auto',
-    //     resize: true
-    // });
     
 });
